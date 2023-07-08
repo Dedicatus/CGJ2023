fb3f9663a1f1a44f9d1ebd938f858aafb5eece33
danyow <i.zuucol@gmail.com>
2023-07-08 11:52:03 +0800

    color

---
+++ Assets/Scripts/RequirementColor.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = nameof(RequirementColor), menuName = nameof(RequirementColor), order = 0)]
+public class RequirementColor : SerializedScriptableObject
+{
+    public Dictionary<Requirement, Color> Colors;
+}
