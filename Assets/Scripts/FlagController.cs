7c1191c72744ac268301404ecf68dbb84bf5c78d
Restraint7 <restraint7s@gmail.com>
2023-07-08 14:52:10 +0800

    Flag

--- Assets/Scripts/FlagController.cs
+++ Assets/Scripts/FlagController.cs
@@ -9,6 +9,8 @@ public class FlagController : MonoBehaviour
     public Requirement requirement;
     public float formationRadius;
     public float attractRadius;
+    public float keepTime=9999;
+    private float currentTime=9999;
 
     private void OnEnable()
     {
@@ -17,12 +19,16 @@ public class FlagController : MonoBehaviour
 
     void Start()
     {
-        
+        currentTime = keepTime;
     }
 
     void Update()
     {
-        
+        currentTime -= Time.deltaTime;
+        if (currentTime <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
     public bool InFormationRange(Vector3 position)
     {
@@ -31,5 +37,10 @@ public class FlagController : MonoBehaviour
     public bool InAttarctRange(Vector3 position)
     {
         return (position - transform.position).magnitude < attractRadius;
+    }
+
+    public void InitFlag(Requirement commingRequirement)
+    {
+        requirement = commingRequirement;
     }
 }
